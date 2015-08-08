f34346f478370ed1e1c123386d7d188a119bf300
Eduardo Iturra S <edu.iturra.analista@gmail.com>
2015-08-08 13:51:17 -0400

    Tercero commit

---
+++ EntityFrameWork/MvcApplication3/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication3.Models;
+
+namespace MvcApplication3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+       
+        public ActionResult Details()
+        {
+            Employee employee = new Employee()
+            {
+                EmployeeId = 101,
+                Gender = "Male",
+                City = "Melipilla",
+                Name = "Eduardo"
+
+            };
+            return View(employee);
+        }
+
+        public ActionResult Entity()
+        {
+            EmployeeContext context = new EmployeeContext();
+            Employee emplo = context.Employees.Single();
+            return View(emplo);
+        }
+
+    }
+}
