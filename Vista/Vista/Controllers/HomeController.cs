e6e2c590f8e4bd9afe24052840baab1f5ac6a09a
Eduardo Iturra S <edu.iturra.analista@gmail.com>
2015-08-08 13:48:40 -0400

    Segundo commit

---
+++ Vista/Vista/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vista.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Pais = new List<string>()
+            { 
+                "India",
+                "UK",
+                "Canada",
+                "US"
+            };
+
+            ViewData["Paises"] = new List<string>()
+            { 
+                "India",
+                "UK",
+                "Canada",
+                "US"
+            };
+
+            return View();
+        }
+
+        
+
+    }
+}
