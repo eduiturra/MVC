acb7be5cd3c1fc28dc3c033a13014f69383bc026
Eduardo Iturra S <edu.iturra.analista@gmail.com>
2015-08-08 22:33:33 -0400

    Secto commit

---
+++ 6_Create/6_Create/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _6_Create.Models;
+
+namespace _6_Create.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            _6_Create.Models.Context con = new Context();
+            List<Departament> dep = con.Departamentos.ToList();
+            return View(dep);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            
+            return View();
+        }
+
+    }
+}
